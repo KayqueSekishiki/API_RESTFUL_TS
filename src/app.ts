87aeb2a79732a8a44fba9c3a3a8198978cada3ed
Kayque Lopes <104032451+KayqueSekishiki@users.noreply.github.com>
2023-11-21 20:12:00 -0300

    db Configurado!

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,19 @@
-console.log("Hello World!");
+require("dotenv").config();
+
+import express from "express";
+import config from "config";
+
+const app = express();
+
+app.use(express.json());
+import db from "../config/db";
+import router from "./router";
+
+app.use("/api/", router);
+const port = config.get<number>("port");
+
+app.listen(port, async () => {
+  await db();
+
+  console.log(`Aplicação está funcionando na porta: ${port}`);
+});
