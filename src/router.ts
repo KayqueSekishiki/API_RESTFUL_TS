b29eb7dcff53fae97c63eb2542b18dc435d397a8
Kayque Lopes <104032451+KayqueSekishiki@users.noreply.github.com>
2023-11-21 21:18:47 -0300

    Create Middlewares for Validationsgit add .

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,13 @@
 import { Router, Request, Response } from "express";
 import { createMovie } from "./controllers/movieController";
 
+import { movieCreateValidation } from "./middleware/movieValidation";
+import { validate } from "./middleware/handleValidation";
+
 const router = Router();
 
 export default router
   .get("/teste", (req: Request, res: Response) => {
     res.status(200).send("API Working");
   })
-  .post("/movie", createMovie);
+  .post("/movie", movieCreateValidation, validate, createMovie);
